4720832cc48382ebe4e4e6f4d7d84a5c252b6699
minji-yun <minpaper.dev@gmail.com>
2023-08-03 19:02:08 +0900

    [feat] firebase 연동

---
+++ src/Firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCa1hULO1CLfwLrzpM9_60-qZhh2Ihy-gI",
+  authDomain: "scentsation-4e0af.firebaseapp.com",
+  projectId: "scentsation-4e0af",
+  storageBucket: "scentsation-4e0af.appspot.com",
+  messagingSenderId: "785492086425",
+  appId: "1:785492086425:web:904cccdde089533904b208"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app)
