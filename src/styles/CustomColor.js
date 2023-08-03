629476284a0df129639cb0873b825b4d5aaffd9b
minji-yun <minpaper.dev@gmail.com>
2023-08-03 19:56:40 +0900

    [style] color palette

---
+++ src/styles/CustomColor.js
@@ -0,0 +1,26 @@
+const palette = {
+    Brown100 : '#E9DACD',
+    Brown200 : '#BDB1A6',
+    Brown300 : '#F3F0DF',
+    Brown400 : '#CFCCB8',
+    Brown500 : '#636158',
+
+    Gray100 : '#A0A0A0',
+    Gray200 : '#828282',
+
+    Red100 : '#F24726',
+    Red200 : '#E75401',
+
+    Pink100 : '#FFCECE',
+
+    Blue100 : '#CEE2FF',
+    Blue200 : '#68C0FF',
+
+    Yellow100 : '#FEF445',
+
+    Green100 : '#CEE741'
+
+
+  };
+  
+  export default palette;
