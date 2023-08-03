6dc7901f7108ff003aaaa4b1f0e92b1be79a1d6f
minji-yun <minpaper.dev@gmail.com>
2023-08-03 19:56:52 +0900

    [style] Custom Font

---
+++ src/styles/CustomFont.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Text = styled.span`
+  font-weight : ${props =>(props.weight ? props.weight : 400)};
+  font-size: ${props => (props.size ? props.size : 12)}px;
+  color: ${props => (props.color ? props.color : '#292A2B')};
+  margin-right: ${props => (props.marginRi ? props.marginRi : 0)}px;
+  margin-left: ${props => (props.marginLf ? props.marginLf : 0)}px;
+  margin-bottom: ${props => (props.marginBt ? props.marginBt : 0)}px;
+  margin-top: ${props => (props.marginTop ? props.marginTop : 0)}px;
+  text-align: ${props =>
+    props.center ? 'center' : props.right ? 'right' : 'left'};
+  opacity: ${props => (props.opacity ? props.opacity : 1)};
+  text-decoration-line: ${props =>
+    props.textDecoLine ? props.textDecoLine : 'none'};
+  text-decoration-color: ${props =>
+    props.textDecoColor ? props.textDecoColor : '#292A2B'};
+`;
+
+export default ({
+  type,
+  size,
+  color,
+  content,
+  weight,
+  marginRi,
+  marginLf,
+  marginBt,
+  marginTop,
+  center,
+  right,
+  opacity,
+  textDecoLine,
+  textDecoColor,
+  style
+}) => {
+  
+  const checkType = () => {
+    
+      return (
+        <Text
+          size={size}
+          color={color}
+          weight={weight}
+          marginRi={marginRi}
+          marginLf={marginLf}
+          marginBt={marginBt}
+          marginTop={marginTop}
+          center={center}
+          right={right}
+          opacity={opacity}
+          textDecoLine={textDecoLine}
+          textDecoColor={textDecoColor}
+          style={{ ...style }}
+        >
+          {content}
+        </Text>
+      );
+    
+  };
+
+  return checkType(type);
+};
