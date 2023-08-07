28d2946ea8d944f99c925e52a79bc3b3323eff65
minji-yun <minpaper.dev@gmail.com>
2023-08-07 20:11:52 +0900

    [feat] router 설정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Main from './pages/Main';
+import { styled } from 'styled-components';
+import Login from './pages/Login';
+import Signup from './pages/SignUp';
+import app from './Firebase'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Main />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+      </BrowserRouter>
+    </Container>
   );
 }
+
+
+const Container = styled.div`
+width : 480px;
+margin : 0 auto;
+overflow : hidden;
+display : flex; 
+flex-direction : column;
+min-height : 100vh;
+`
 
 export default App;
