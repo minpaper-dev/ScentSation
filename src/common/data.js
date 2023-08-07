786db7eeac351343d69e0d0dace4b41af41d0e68
minji-yun <minpaper.dev@gmail.com>
2023-08-07 20:12:44 +0900

    [etc] 전역 data

---
+++ src/common/data.js
@@ -0,0 +1,31 @@
+export const categories = [
+  {
+    icon: '🍊',
+    title: '시트러스',
+  },
+  {
+    icon: '🌷',
+    title: '플로럴',
+  },
+  {
+    icon: '🪵',
+    title: '우디',
+  },
+  {
+    icon: '🧂',
+    title: '스파이시',
+  },
+  {
+    icon: '🍼',
+    title: '파우더리',
+  },
+  {
+    icon: '🦌',
+    title: '머스크',
+  },
+]
+
+export const genderList = [
+  { title: '남자', value: 'male' },
+  { title: '여자', value: 'feMail' },
+]
