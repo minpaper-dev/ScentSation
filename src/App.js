dc6954d8e761bb35db08e04b63bf03c6b4b8da07
minji-yun <minpaper.dev@gmail.com>
2023-08-08 18:18:00 +0900

    [etc] scroll to top 적용

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Main from './pages/Main';
-import { styled } from 'styled-components';
-import Login from './pages/Login';
-import Signup from './pages/SignUp';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Main from './pages/Main'
+import { styled } from 'styled-components'
+import { ScrollToTop } from './components/ScrollToTop'
+import Login from './pages/Login'
+import Signup from './pages/SignUp'
 import app from './Firebase'
+import MyPage from './pages/MyPage'
 
 function App() {
   return (
     <Container className="App">
       <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Main />}/>
-        <Route path='/login' element={<Login />}/>
-        <Route path='/signup' element={<Signup />} />
-      </Routes>
+        <ScrollToTop />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
       </BrowserRouter>
     </Container>
-  );
+  )
 }
 
-
 const Container = styled.div`
-width : 480px;
-margin : 0 auto;
-overflow : hidden;
-display : flex; 
-flex-direction : column;
-min-height : 100vh;
+  width: 480px;
+  margin: 0 auto;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
 `
 
-export default App;
+export default App
