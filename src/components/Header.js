a7a53e958be320c931349ddfba710d7d6049ea2d
minji-yun <minpaper.dev@gmail.com>
2023-08-08 18:17:46 +0900

    [feat] 로그인 여부 분기처리

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,57 @@
-import React from "react"
-import { styled } from "styled-components"
+import React, { useEffect, useState } from 'react'
+import { styled } from 'styled-components'
+import { Link } from 'react-router-dom'
+import palette from '../styles/CustomColor'
+import CustomFont from '../styles/CustomFont'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 const Header = () => {
-    return (
-        <Container>
-            <Logo>
-            ScentSation
-            </Logo>
-        </Container>
-    )
+  const auth = getAuth()
+
+  const [userId, setUserId] = useState('')
+
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const uid = user.uid
+        console.log(user, uid)
+        setUserId(uid)
+        // ...
+      } else {
+        // User is signed out
+        // ...
+      }
+    })
+  }, [])
+
+  return (
+    <Container>
+      <Logo></Logo>
+      <Link to={userId ? '/mypage' : '/login'}>
+        <Button>
+          <CustomFont content={userId ? 'MyPage' : 'Login'} />
+        </Button>
+      </Link>
+    </Container>
+  )
 }
 
 const Container = styled.header`
-    background-color : white;
-    min-height : 100px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: white;
+  min-height: 100px;
+  padding: 18px;
+  padding-top: 40px;
 `
 
 const Logo = styled.div`
-    color : #E9DACD;
-    padding : 18px;
-    padding-top : 40px
+  color: #e9dacd;
 `
+
+const Button = styled.button``
 
 export default Header
