4b1abd13f73e59179749a7ae1128215061d1cc05
minji-yun <minpaper.dev@gmail.com>
2023-08-10 20:14:07 +0900

    [etc] size 상수화

---
+++ src/common/size.js
@@ -0,0 +1,1 @@
+export const MAIN_CATEGORY_WIDTH = 26
--- src/components/Main/MainComponent.js
+++ src/components/Main/MainComponent.js
@@ -1,25 +1,27 @@
 import React from 'react'
 import { styled } from 'styled-components'
 import CustomFont from '../../styles/CustomFont'
+import { MAIN_CATEGORY_WIDTH } from '../../common/size'
 
 const MainComponent = ({ title, component }) => {
   return (
     <Container>
-      <CustomFont weight={800} content={title} marginBt={25} />
+      <CustomFont size={1.2} weight={800} content={title} $marginBt={2} />
       {component}
     </Container>
   )
 }
 
 const Container = styled.div`
+  max-width: ${`${MAIN_CATEGORY_WIDTH}rem`};
   display: flex;
   flex-direction: column;
   align-items: center;
 
   background-color: white;
   border-radius: 16px;
-  margin: 20px 25px 0px;
-  padding: 20px 24px;
+  margin: 2rem 0px;
+  padding: 2rem;
 `
 
 export default MainComponent
