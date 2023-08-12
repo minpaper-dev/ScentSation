89e4152c8a1351aa46d57299a5a2680d19869e47
minji-yun <minpaper.dev@gmail.com>
2023-08-12 22:02:00 +0900

    [etc] data 추가

--- src/common/data.js
+++ src/common/data.js
@@ -1,3 +1,5 @@
+import palette from '../styles/CustomColor'
+
 export const categories = [
   {
     id: 0,
@@ -36,4 +38,39 @@ export const genderList = [
   { title: '여자', value: 'feMail' },
 ]
 
+export const FILTER_CATEGORY = [
+  '전체',
+  '플로럴',
+  '시트러스',
+  '우디',
+  '스파이시',
+  '파우더리',
+  '머스크',
+]
+
+export const REVIEW_DATA_COLOR = {
+  male: palette.Blue100,
+  female: palette.Pink100,
+  neutral: palette.Gray100,
+  spring: palette.Yellow100,
+  summer: palette.Green100,
+  autumn: palette.Red200,
+  winter: palette.Blue200,
+  cologne: palette.Brown300,
+  toilette: palette.Brown400,
+  perfume: palette.Brown500,
+}
+
+export const REVIEW_DATA_TEXT = {
+  male: '남성에게 더 잘어울려요',
+  female: '여성에게 더 잘어울려요',
+  neutral: '성별과 상관 없어요',
+  spring: '봄에 뿌리기 좋아요',
+  summer: '여름에 뿌리기 좋아요',
+  autumn: '가을에 뿌리기 좋아요',
+  winter: '겨울에 뿌리기 좋아요',
+  cologne: '향이 1~2시간 지속돼요',
+  toilette: '향이 3~4시간 지속돼요',
+  perfume: '향이 5~6시간 지속돼요',
+}
 export const NO_HEADER = ['/signup', '/login']
