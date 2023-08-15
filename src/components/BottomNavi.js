df81a33a4ff2fbabb4bb7113d6c379fd47ccddad
minji-yun <minpaper.dev@gmail.com>
2023-08-15 17:42:53 +0900

    [feat] Header, Bottom Navi 구현

---
+++ src/components/BottomNavi.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { styled } from 'styled-components'
+import palette from '../styles/CustomColor'
+import { Link, useNavigate } from 'react-router-dom'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
+const BottomNavi = () => {
+  const auth = getAuth()
+  const navigate = useNavigate()
+
+  const goProfile = () => {
+    let userId = ''
+    onAuthStateChanged(auth, user => {
+      console.log(user)
+      if (user) {
+        userId = user.uid
+        navigate('/mypage')
+      } else {
+        navigate('/login')
+      }
+    })
+    console.log(userId)
+  }
+
+  return (
+    <Container>
+      <StyleLink to={'/'}>
+        <Icon>홈</Icon>
+      </StyleLink>
+      <StyleLink to={'/'}>
+        <Icon>리뷰</Icon>
+      </StyleLink>
+      <Button onClick={goProfile}>마이페이지</Button>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 480px;
+  height: 3rem;
+  background-color: white;
+  border-top: 1px solid ${palette.Gray300};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const StyleLink = styled(Link)`
+  flex: 1;
+`
+
+const Icon = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: calc(480 / 3) px;
+`
+
+const Button = styled.button`
+  flex: 1;
+`
+
+export default BottomNavi
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,26 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { styled } from 'styled-components'
-import { Link } from 'react-router-dom'
-import palette from '../styles/CustomColor'
+import { useNavigate } from 'react-router-dom'
 import CustomFont from '../styles/CustomFont'
-import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { NO_HEADER } from '../common/data'
 
-const Header = () => {
-  const auth = getAuth()
+const Header = ({ pageName }) => {
+  const navigate = useNavigate()
 
-  const [userId, setUserId] = useState('')
+  const goBack = () => {
+    navigate(-1)
+  }
 
-  useEffect(() => {
-    onAuthStateChanged(auth, user => {
-      if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/auth.user
-        const uid = user.uid
-        console.log(user, uid)
-        setUserId(uid)
-        // ...
-      } else {
-        // User is signed out
-        // ...
-      }
-    })
-  }, [])
+  const goSearch = () => {
+    navigate('/search')
+  }
 
   if (NO_HEADER.includes(window.location.pathname)) return null
   return (
     <Container>
-      <Logo></Logo>
-      <Link to={userId ? '/mypage' : '/login'}>
-        <Button>
-          <CustomFont content={userId ? 'MyPage' : 'Login'} />
-        </Button>
-      </Link>
+      <Icon onClick={goBack}>뒤로</Icon>
+      <CustomFont content={pageName} weight={800} />
+      <Icon onClick={goSearch}>검색</Icon>
     </Container>
   )
 }
@@ -45,15 +30,10 @@ const Container = styled.header`
   align-items: center;
   justify-content: space-between;
   background-color: white;
-  min-height: 100px;
-  padding: 18px;
-  padding-top: 40px;
+  min-height: 5rem;
+  padding: 2rem;
 `
 
-const Logo = styled.div`
-  color: #e9dacd;
-`
-
-const Button = styled.button``
+const Icon = styled.button``
 
 export default Header
