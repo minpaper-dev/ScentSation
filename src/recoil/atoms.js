0c59c4110b575d66591240c630ad6e78722c65e4
minji-yun <minpaper.dev@gmail.com>
2023-08-20 21:30:47 +0900

    [feat] recoil 설정

---
+++ src/recoil/atoms.js
@@ -0,0 +1,11 @@
+import { atom } from 'recoil'
+
+export const myVotePerfumeState = atom({
+  key: 'myVotePerfumeState',
+  default: [],
+})
+
+export const myInfoState = atom({
+  key: 'myInfoState',
+  default: {},
+})
