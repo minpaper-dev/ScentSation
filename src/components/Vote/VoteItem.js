ea811e4fade4b81a583b410520762b2163522658
minji-yun <minpaper.dev@gmail.com>
2023-08-20 21:30:16 +0900

    [fix] 실제 데이터 반영

--- src/components/Vote/VoteItem.js
+++ src/components/Vote/VoteItem.js
@@ -3,46 +3,56 @@ import { styled } from 'styled-components'
 import CustomFont from '../../styles/CustomFont'
 import palette from '../../styles/CustomColor'
 import ProfileItem from '../Profile/ProfileItem'
+import VoteProduct from './VoteProduct'
+import { useNavigate } from 'react-router-dom'
 
 const VoteItem = ({ data }) => {
   return (
     <>
-      <ProfileItem />
-      <CustomFont
-        content={
-          '안녕하세요 제가 이런저런 이유로 향수를 고민중인데 투표 부탁드려요'
-        }
-        $marginBt={1}
-        $marginTop={1}
-      />
-      <VoteList>
-        <VoteBox>
-          <CustomFont content={JSON.stringify(data)} />
-        </VoteBox>
-        <VoteBox>
-          <CustomFont content={'어나더 13'} />
-        </VoteBox>
-      </VoteList>
+      <Container key={data.id}>
+        <ProfileItem data={data.userInfo} />
+        <CustomFont content={data.description} $marginTop={1} $marginBt={1} />
+        <WrapVoteButton>
+          <VoteButton>
+            <VoteProduct data={data.perfume[0]} />
+          </VoteButton>
+          <VoteButton>
+            <VoteProduct data={data.perfume[1]} />
+          </VoteButton>
+        </WrapVoteButton>
+      </Container>
     </>
   )
 }
 
-const VoteList = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 25px;
-`
-
-const VoteBox = styled.div`
+const Container = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100%;
+  margin: 0 auto;
+`
+
+const WrapVoteButton = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const VoteButton = styled.button`
+  width: 45%;
+  display: flex;
   align-items: center;
-  background-color: white;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.2);
-  border: 1px solid ${palette.Gray300};
-  border-radius: 8px;
-  padding: 10px 0px;
+  background-color: white;
+  padding: 2rem 0.5rem;
+  border-radius: 1rem;
+  margin-bottom: 19px;
   cursor: pointer;
+
+  &:hover,
+  &:active,
+  &:focus {
+    transform: scale(1.05);
+  }
 `
 
 export default VoteItem
