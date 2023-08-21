1ee06302446eea4931d6b527cb84c3040d40440c
minji-yun <minpaper.dev@gmail.com>
2023-08-21 18:06:43 +0900

    [etc] 필요없는 코드 삭제

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -221,35 +221,7 @@ const Signup = () => {
     const storage = getStorage()
     const storageRef = ref(storage, 'image/image1.jpg')
     if (profileImage) {
-      const uploadTask = uploadBytesResumable(storageRef, profileImage)
-
-      uploadTask.on(
-        'state_changed',
-        snapshot => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-          console.log('Upload is ' + progress + '% done')
-          switch (snapshot.state) {
-            case 'paused':
-              console.log('Upload is paused')
-              break
-            case 'running':
-              console.log('Upload is running')
-              break
-          }
-        },
-        error => {
-          console.log(error)
-          // Handle unsuccessful uploads
-        },
-        () => {
-          // Handle successful uploads on complete
-          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-          getDownloadURL(uploadTask.snapshot.ref).then(downloadURL => {
-            console.log('File available at', downloadURL)
-          })
-        }
-      )
+      uploadBytesResumable(storageRef, profileImage)
     }
   }
 
