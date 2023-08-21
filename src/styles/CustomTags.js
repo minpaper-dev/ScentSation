c70fede28eb86d183d72af35d9ffd5ecf732de2b
minji-yun <minpaper.dev@gmail.com>
2023-08-21 18:05:06 +0900

    [feat] CustomTag 생성

--- src/styles/CustomTags.js
+++ src/styles/CustomTags.js
@@ -3,19 +3,22 @@ import { styled } from 'styled-components'
 import palette from './CustomColor'
 import CustomFont from './CustomFont'
 
-const CustomTags = ({ content, bgc, $marginRi }) => {
+const CustomTags = ({ content, bgc, $marginRi, onClick }) => {
   return (
-    <Tag bgc={bgc} $marginRi={$marginRi}>
-      <CustomFont size={0.8} content={content} weight={800} color={'white'} />
+    <Tag onClick={() => onClick(content)} bgc={bgc} $marginRi={$marginRi}>
+      <CustomFont size={0.6} content={content} weight={800} color={'white'} />
     </Tag>
   )
 }
 
-const Tag = styled.div`
-  width: auto;
+const Tag = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 4rem;
   background-color: ${props => (props.bgc ? props.bgc : palette.Brown200)};
-  border-radius: 6px;
-  padding: 5px 15px;
+  border-radius: 1rem;
+  padding: 0.4rem 0.4rem;
   margin-right: ${props => (props.$marginRi ? props.$marginRi : 0.5)}rem;
 `
 
