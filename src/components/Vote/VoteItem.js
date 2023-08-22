423839f34b22a6b0c3012118c3bc38ffd067accd
minji-yun <minpaper.dev@gmail.com>
2023-08-22 19:01:08 +0900

    [feat] 투표 완료 시 UI

--- src/components/Vote/VoteItem.js
+++ src/components/Vote/VoteItem.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { styled } from 'styled-components'
 import CustomFont from '../../styles/CustomFont'
 import ProfileItem from '../Profile/ProfileItem'
 import VoteProduct from './VoteProduct'
+import palette from '../../styles/CustomColor'
 
 const VoteItem = ({ data }) => {
+  const [isVote, setIsVote] = useState(false)
+  const [selectedItem, setSelectedItem] = useState('')
+
+  const onVote = perfume => {
+    console.log(perfume)
+    setIsVote(true)
+    setSelectedItem(perfume.id)
+  }
   return (
     <>
       <Container key={data.id}>
@@ -16,10 +25,28 @@ const VoteItem = ({ data }) => {
           $marginBt={2}
         />
         <WrapVoteButton>
-          <VoteButton>
+          <VoteButton
+            onClick={() => onVote(data.perfume[0])}
+            isSelected={selectedItem === data.perfume[0].id}
+          >
             <VoteProduct data={data.perfume[0]} />
+
+            <VoteResult isVote={isVote} />
+            {isVote && (
+              <>
+                <CustomFont
+                  size={1.6}
+                  color={palette.Brown200}
+                  content={'50%'}
+                  weight={800}
+                />
+              </>
+            )}
           </VoteButton>
-          <VoteButton>
+          <VoteButton
+            onClick={() => onVote(data.perfume[1])}
+            isSelected={selectedItem === data.perfume[1].id}
+          >
             <VoteProduct data={data.perfume[1]} />
           </VoteButton>
         </WrapVoteButton>
@@ -37,19 +64,36 @@ const Container = styled.div`
 
 const WrapVoteButton = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
 `
 
 const VoteButton = styled.button`
-  width: 48%;
+  flex: 1;
+  position: relative;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.2);
   background-color: white;
-  padding: 2rem 0.5rem;
+  padding: 2rem;
   border-radius: 1rem;
+  border: ${props => (props.isSelected ? `2px solid ${palette.Brown200}` : 0)};
+
   margin-bottom: 2rem;
   cursor: pointer;
+`
+
+const VoteResult = styled.div`
+  position: absolute;
+  left: 0;
+  width: 70%;
+  height: 100%;
+  background-color: ${palette.Brown200};
+  opacity: 0.3;
+  border-radius: inherit;
+  width: ${props => (props.isVote ? '50%' : '0')};
+  transition: width 0.5s ease; /* 애니메이션 효과 설정 */
 `
 
 export default VoteItem
--- src/components/Vote/VoteProduct.js
+++ src/components/Vote/VoteProduct.js
@@ -3,18 +3,23 @@ import CustomFont from '../../styles/CustomFont'
 
 const VoteProduct = ({ data }) => {
   return (
-    <>
+    <Container>
       <ProductImage src={data.image} />
       <ProductInfo>
         <CustomFont content={data.brand} $marginBt={0.5} />
         <CustomFont content={data.name} />
       </ProductInfo>
-    </>
+    </Container>
   )
 }
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+`
+
 const ProductImage = styled.img`
-  width: 40%;
+  width: 25%;
   height: auto;
   background-color: white;
 `
@@ -22,6 +27,7 @@ const ProductImage = styled.img`
 const ProductInfo = styled.div`
   display: flex;
   flex-direction: column;
+  margin-left: 1rem;
 `
 
 export default VoteProduct
