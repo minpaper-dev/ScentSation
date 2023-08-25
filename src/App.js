e05287bddd8bdd98e9afa8439d7095c6d7b1109c
minji-yun <minpaper.dev@gmail.com>
2023-08-26 02:45:30 +0900

    [etc] 페이지 추가

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { myInfoState } from './recoil/atoms'
 import { MY_UID } from './common/localstorage'
 import NotFound from './pages/NotFound'
+import Brand from './pages/Brand'
 
 function App() {
   const auth = getAuth()
@@ -35,159 +36,105 @@ function App() {
   const { addData, getDataWithId } = useFirestore()
 
   const data = [
-    // {
-    //   name: '블루 드 샤넬 오 드 빠르펭',
-    //   brand: '샤넬',
-    //   size: 50,
-    //   price: 135000,
-    //   image:
-    //     'https://dn5hzapyfrpio.cloudfront.net/product/178/178bac20-2989-11eb-953b-bd59e745a95b.png?w=456',
-    //   category: ['머스크', '우디'],
-    // },
-    // {
-    //   name: '미스 디올 블루밍 부케 롤러 펄 오 드 뚜왈렛',
-    //   brand: '디올',
-    //   size: 20,
-    //   price: 71000,
-    //   image:
-    //     'https://dn5hzapyfrpio.cloudfront.net/product/ad0/ad012140-0c30-11ee-8d6a-939024f3fb58.png?w=456',
-    //   category: ['머스크', '플로럴'],
-    // },
-    // {
-    //   name: '끌로에 오 드 퍼퓸',
-    //   brand: '끌로에',
-    //   size: 50,
-    //   price: 150000,
-    //   image:
-    //     'https://dn5hzapyfrpio.cloudfront.net/product/5fb/5fb4f6a0-08fc-11ee-9c6c-41fb11a103a9.jpeg?w=456',
-    //   category: ['플로럴'],
-    // },
-    // {
-    //   name: '어벤투스',
-    //   brand: '크리드',
-    //   size: 75,
-    //   price: 348000,
-    //   image:
-    //     'https://dn5hzapyfrpio.cloudfront.net/home/glowmee/upload/20140623/1403602059901.jpg?w=456',
-    //   category: ['프루티', '머스크'],
-    // },
-    // {
-    //   name: '아쿠아 알레고리아 페라 그라니타 오 드 뚜왈렛 ',
-    //   brand: '겔랑',
-    //   size: 75,
-    //   price: 164000,
-    //   image:
-    //     'https://dn5hzapyfrpio.cloudfront.net/product/b85/b85919e0-edea-11ec-8ad7-a3acf1cd4d18.jpeg?w=456',
-    //   category: ['프루티', '시트러스'],
-    // },
-    // {
-    //   name: '샹스 오 땅드르 오 드 빠르펭',
-    //   brand: '샤넬',
-    //   size: 50,
-    //   price: 179000,
-    //   image:
-    //     'https://dn5hzapyfrpio.cloudfront.net/product/eec/eec994f0-f933-11ed-9de5-29b7891c15cf.jpeg?w=456',
-    //   category: ['프루티', '플로럴'],
-    // },
-    // {
-    //   name: '터치 포 맨 오 드 뚜왈렛',
-    //   brand: '버버리',
-    //   size: 50,
-    //   price: 84000,
-    //   image:
-    //     'https://dn5hzapyfrpio.cloudfront.net/product/cdc/cdc39830-be58-11ec-9fb3-b12de47e278a.png?w=456',
-    //   category: ['프레쉬', '머스크'],
-    // },
-    // {
-    //   name: '미스 디올 오 드 퍼퓸',
-    //   brand: '디올',
-    //   size: 50,
-    //   price: 182000,
-    //   image:
-    //     'https://dn5hzapyfrpio.cloudfront.net/product/d1b/d1bc97e0-58b7-11ec-8614-1b72223f29b9.jpeg?w=456',
-    //   category: ['플로럴', '머스크'],
-    // },
-    // {
-    //   name: '테싯 오 드 퍼퓸',
-    //   brand: '이솝',
-    //   size: 50,
-    //   price: 150000,
-    //   image:
-    //     'https://dn5hzapyfrpio.cloudfront.net/product/380/380e40c0-9900-11eb-a2c4-eb2881e350fc.png?w=456',
-    //   category: ['프레쉬', '시트러스', '그린'],
-    // },
-    // {
-    //   name: '라 튤립 오 드 퍼퓸',
-    //   brand: '바이레도',
-    //   size: 50,
-    //   price: 260000,
-    //   image:
-    //     'https://dn5hzapyfrpio.cloudfront.net/home/glowmee/upload/product/20200929/1601344105049.png?w=456',
-    //   category: ['플로럴'],
-    // },
-    // {
-    //   name: '루나 오 드 뚜왈렛',
-    //   brand: '펜할리곤스',
-    //   size: 100,
-    //   price: 286000,
-    //   image:
-    //     'https://dn5hzapyfrpio.cloudfront.net/product/386/386bf0b0-13e5-11ed-a67e-eb6b6362548f.png?w=456',
-    //   category: ['플로럴', '시트러스'],
-    // },
-    // {
-    //   name: '로즈 오브 노 맨즈 랜드 오 드 퍼퓸',
-    //   brand: '바이레도',
-    //   size: 50,
-    //   price: 260000,
-    //   image:
-    //     'https://dn5hzapyfrpio.cloudfront.net/product/ca0/ca00d300-267a-11ec-94d6-b5b818462517.jpeg?w=456',
-    //   category: ['머스크', '프루티', '플로럴'],
-    // },
-    // {
-    //   name: '오 로즈 오 드 뚜왈렛',
-    //   brand: '딥티크',
-    //   size: 50,
-    //   price: 165000,
-    //   image:
-    //     'https://dn5hzapyfrpio.cloudfront.net/product/bd7/bd7f7500-ea55-11ed-8df0-b549d3493d43.png?w=456',
-    //   category: ['프루티', '플로럴'],
-    // },
-    // {
-    //   name: '롬브르 단 로 오 드 퍼퓸',
-    //   brand: '딥티크',
-    //   size: 75,
-    //   price: 269000,
-    //   image:
-    //     'https://dn5hzapyfrpio.cloudfront.net/var/app/current/common/upload/20200221/1582272403813.jpg?w=456',
-    //   category: ['플로럴'],
-    // },
-    // {
-    //   name: '모던 프린세스 오 드 퍼퓸',
-    //   brand: '랑방',
-    //   size: 60,
-    //   price: 90000,
-    //   image:
-    //     'https://dn5hzapyfrpio.cloudfront.net/home/glowmee/upload/product/20200527/1590565152325.png?w=456',
-    //   category: ['머스크', '프루티', '플로럴'],
-    // },
-    // {
-    //   name: '퓨어 그레이스 누드 로즈 프레그런스 오 드 뚜왈렛',
-    //   brand: '필로소피',
-    //   size: 60,
-    //   price: 69000,
-    //   image:
-    //     'https://dn5hzapyfrpio.cloudfront.net/home/glowmee/upload/20190108/1546909633575.png?w=456',
-    //   category: ['플로럴'],
-    // },
-    // {
-    //   name: '퓨어 그레이스 누드 로즈 프레그런스 오 드 뚜왈렛',
-    //   brand: '필로소피',
-    //   size: 60,
-    //   price: 69000,
-    //   image:
-    //     'https://dn5hzapyfrpio.cloudfront.net/home/glowmee/upload/20190108/1546909633575.png?w=456',
-    //   category: ['플로럴'],
-    // },
+    {
+      name: '라이트 에센스 오 드 뚜왈렛',
+      brand: '페라리',
+      size: 125,
+      price: 104000,
+      image:
+        'https://dn5hzapyfrpio.cloudfront.net/product/6f9/6f9b0c80-4b4d-11ec-9b8a-7d5c8332958d.jpeg?w=456',
+      category: ['프루티', '시트러스'],
+    },
+    {
+      name: '미스 디올 블루밍 부케 오 드 뚜왈렛',
+      brand: '디올',
+      size: 50,
+      price: 155000,
+      image:
+        'https://dn5hzapyfrpio.cloudfront.net/product/eff/efffa8f0-ea56-11ed-9b83-c97f8f5e832b.png?w=456',
+      category: ['플로럴'],
+    },
+    {
+      name: '잉글리쉬 페어 앤 프리지아 코롱',
+      brand: '조말론',
+      size: 100,
+      price: 218000,
+      image:
+        'https://dn5hzapyfrpio.cloudfront.net/home/glowmee/upload/product/20200527/1590570757967.PNG?w=456',
+      category: ['플로럴', '프루티'],
+    },
+    {
+      name: '잉글리쉬 페어 앤 프리지아 코롱',
+      brand: '조말론',
+      size: 100,
+      price: 218000,
+      image:
+        'https://dn5hzapyfrpio.cloudfront.net/home/glowmee/upload/product/20200527/1590570757967.PNG?w=456',
+      category: ['플로럴', '프루티'],
+    },
+    {
+      name: '가브리엘 샤넬 오 드 빠르펭',
+      brand: '샤넬',
+      size: 50,
+      price: 179000,
+      image:
+        'https://dn5hzapyfrpio.cloudfront.net/home/glowmee/upload/20170831/1504169593130.png?w=456',
+      category: ['플로럴', '프루티'],
+    },
+    {
+      name: '알뤼르 옴므 스포츠 오 드 뚜왈렛',
+      brand: '샤넬',
+      size: 50,
+      price: 120000,
+      image:
+        'https://dn5hzapyfrpio.cloudfront.net/product/487/48728700-990a-11eb-b73d-4be3072764b5.png?w=456',
+      category: ['플로럴', '프루티'],
+    },
+    {
+      name: '알뤼르 옴므 스포츠 오 드 뚜왈렛',
+      brand: '샤넬',
+      size: 50,
+      price: 120000,
+      image:
+        'https://dn5hzapyfrpio.cloudfront.net/product/487/48728700-990a-11eb-b73d-4be3072764b5.png?w=456',
+      category: ['플로럴', '프루티'],
+    },
+    {
+      name: '로즈 폼퐁 오 드 뚜왈렛',
+      brand: '구딸 파리',
+      size: 50,
+      price: 210000,
+      image:
+        'https://dn5hzapyfrpio.cloudfront.net/product/f0d/f0d12a20-0f3f-11ee-abf3-694b84207787.jpeg?w=456',
+      category: ['플로럴', '프루티'],
+    },
+    {
+      name: '엉 마뗑 도하주 오 드 뚜왈렛',
+      brand: '구딸 파리',
+      size: 50,
+      price: 210000,
+      image:
+        'https://dn5hzapyfrpio.cloudfront.net/product/c03/c0341470-0f46-11ee-a093-1fedeebf5a3b.jpeg?w=456',
+      category: ['플로럴', '프루티', '프레쉬'],
+    },
+    {
+      name: '릴 오 떼 오 드 뚜왈렛',
+      brand: '구딸 파리',
+      size: 100,
+      price: 290000,
+      image:
+        'https://dn5hzapyfrpio.cloudfront.net/product/a87/a87caa50-0f40-11ee-98ce-a5be4f9afa8b.jpeg?w=456',
+      category: ['시트러스', '프레쉬', '그린'],
+    },
+    {
+      name: '릴 오 떼 오 드 뚜왈렛',
+      brand: '구딸 파리',
+      size: 100,
+      price: 290000,
+      image:
+        'https://dn5hzapyfrpio.cloudfront.net/product/a87/a87caa50-0f40-11ee-98ce-a5be4f9afa8b.jpeg?w=456',
+      category: ['시트러스', '프레쉬', '그린'],
+    },
   ]
 
   const [, setMyInfo] = useRecoilState(myInfoState)
@@ -204,8 +151,12 @@ function App() {
     })
   }, [])
 
+  // useEffect(() => {
+  //   data.map(v => addData_(v))
+  // }, [])
+
   const addData_ = async v => {
-    const result = await addData('product', '', v)
+    // const result = await addData('product', '', v)
     console.log(v)
   }
 
@@ -285,6 +236,10 @@ function App() {
               <Route
                 path="/review/edit"
                 element={<AnimatedPage element={<ReviewEdit />} />}
+              />
+              <Route
+                path="/brand/:id"
+                element={<AnimatedPage element={<Brand />} />}
               />
               <Route
                 path="*"
